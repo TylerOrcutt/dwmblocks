13023280085b6e8d93fa59392c80504f80bcc794
TylerOrcutt <tyorcutt@gmail.com>
2020-12-05 01:22:47 -0500

    dwmblocks

---
+++ config.h
@@ -0,0 +1,16 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+
+	{"",	"transmission-rc -i3-t",	60,	1},
+	{"",	"~/.local/bin/statusbar/forecast",	18000,	5},
+	{"",	"~/.local/bin/statusbar/clock",	30,	1},
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = " ";
+
+// Have dwmblocks automatically recompile and run when you edit this file in
+// vim with the following line in your vimrc/init.vim:
+
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
